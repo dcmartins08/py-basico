9c9b817024df98d78258b7b68ff89b72a610cd1a
dcmartins08 <124939389+dcmartins08@users.noreply.github.com>
2023-03-09 21:24:17 -0300

    Add files via upload

---
+++ Aula de Função.py
@@ -0,0 +1,13 @@
+# exercicío de Função
+# escreva uma função que receba valor em horas e retorna em segundos
+def convert(horas):
+    seg = horas * 24
+    minutos = horas * 12
+    print(seg)
+    print(minutos)
+
+convert(12)
+
+# exercicío de Função
+# escreva uma função que retorne o maior de dois números.
+# valores esperados:
